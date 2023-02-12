68fbcd9df348a22c5fa530eb1ee19e3334510a7c
vk59 <jkostylev592@gmail.com>
2023-02-12 15:12:53 +0300

    Initial commit.

--- app/src/main/java/com/vk59/gotuda/MainFragment.kt
+++ app/src/main/java/com/vk59/gotuda/MainFragment.kt
@@ -1,13 +1,6 @@
 package com.vk59.gotuda
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 
 class MainFragment : Fragment(R.layout.fragment_main) {
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-
-  }
 }
